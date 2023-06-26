933f4b72ef2d8893963e7335d2b569a98ad272f4
Peter Chibunna <peterchibunna@gmail.com>
2023-06-26 18:33:48 +0100

    Task #1: Fix lint issues

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import mongodb from "mongodb";
+import mongodb from 'mongodb';
 
 class DBClient {
   constructor() {
@@ -6,18 +6,21 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
 
-    this.client = new mongodb.MongoClient(`mongodb://${host}:${port}/${database}`,  { useUnifiedTopology: true });
+    this.client = new mongodb.MongoClient(
+      `mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true },
+    );
     this.client.connect();
   }
 
-  isAlive(){
+  isAlive() {
     return this.client.isConnected();
   }
 
-  async nbUsers(){
+  async nbUsers() {
     return this.client.db().collection('users').estimatedDocumentCount();
   }
-  async nbFiles(){
+
+  async nbFiles() {
     return this.client.db().collection('files').estimatedDocumentCount();
   }
 }
