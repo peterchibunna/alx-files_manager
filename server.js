ad80af1474ea7c72c740ba28500dad3bbd6c563f
Peter Chibunna <peterchibunna@gmail.com>
2023-06-27 14:20:55 +0100

    Revert port number

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routes/index';
 
 const server = express();
-const PORT = process.env.PORT ? process.env.PORT : 5001;
+const PORT = process.env.PORT ? process.env.PORT : 5000;
 
 server.use(router);
 
